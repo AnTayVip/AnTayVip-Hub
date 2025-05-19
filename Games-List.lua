c56b6b01da81e04147f5bfd1127c46f31e49f79d
AnTayVip <antayvip@gmail.com>
2025-05-19 08:20:22 +0700

    Update Games-List.lua

--- Games-List.lua
+++ Games-List.lua
@@ -1,7 +1,9 @@
 local Games = {
-  [2753915549] = "http://raw.githubusercontent.com/AnTayVip/AnTayVip-Hub/main/Blox-Fruits.lua",
-  [4442272183] = "http://raw.githubusercontent.com/AnTayVip/AnTayVip-Hub/main/Blox-Fruits.lua",
-  [7449423635] = "http://raw.githubusercontent.com/AnTayVip/AnTayVip-Hub/main/Blox-Fruits.lua",
+    [2753915549] = "http://raw.githubusercontent.com/AnTayVip/AnTayVip-Hub/main/Blox-Fruits.lua",
+    [4442272183] = "http://raw.githubusercontent.com/AnTayVip/AnTayVip-Hub/main/Blox-Fruits.lua",
+    [7449423635] = "http://raw.githubusercontent.com/AnTayVip/AnTayVip-Hub/main/Blox-Fruits.lua",
+    [116495829188952] = "https://raw.githubusercontent.com/AnTanVip/AnTayVip-Hub/main/Dead-Rails.lua",
+    [70876832253163] = "https://raw.githubusercontent.com/AnTanVip/AnTayVip-Hub/main/Dead-Rails.lua",
 }
 
 return Games
